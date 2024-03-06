f4ac8ebcdd2c1ae441a01e7a20eefb9ee300ffb3
Anne Jo <steamengine.a@gmail.com>
2024-03-06 09:11:21 -0500

    Update main method

--- time/src/time/TimeTest.java
+++ time/src/time/TimeTest.java
@@ -1,10 +1,6 @@
 package time;
-import static org.junit.Assert.assertThrows;
+
 import static org.junit.Assert.assertTrue;
-/**
- * Test class for the Time.java file. Will be used to demo Junit testing in SYST 38634
- * Liz Dancy, 2021
- */
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -13,41 +9,86 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class TimeTest {
 
-	/**
-	 * A test on good input. We expect this test to return the correct number of seconds.
-	 */
 	@Test
-	void testGetTotalSecondsGood() 
-	{
-		int seconds = Time.getTotalSeconds("12:05:05");
-		assertTrue("The seconds were not calculated properly", seconds==43505);
+	public void testGetTotalSecondsGood() {
+		int seconds = Time.getTotalSeconds("05:05:05");
+		assertTrue("The seconds were not calculated properly", seconds==18305);
 	}
 	
-	/**
-	 * A method to check that when we enter a String in the wrong format we get a number format exception
-	 */
 	@Test
-	void testGetTotalSecondsBad() 
-	{
+	public void testGetTotalSecondsBad() {
 		assertThrows(StringIndexOutOfBoundsException.class, ()-> {Time.getTotalSeconds("10:00");});
+	}
+	
+	@Test
+	public void testGetTotalSecondsBoundary() {
+		int seconds = Time.getTotalSeconds("23:59:59");
+		assertTrue("The seconds were not calculated properly", seconds==86399);
+	}	
+	
+	// ICE 3 =====================================================================
+	
+	@ParameterizedTest
+	@ValueSource(strings = { "05:05:05:05", "23:59:59:00", "12:05:05:05" })
+	public void testGetMillisecondsGoodAndBoundary(String candidate) {
+		int milliseconds = Time.getMilliseconds(candidate);
+		if (candidate.equals("23:59:59:00"))
+			assertTrue("The milliseconds were not calculated properly", (milliseconds==0));
+		else
+			assertTrue("The milliseconds were not calculated properly", (milliseconds==5));
+	}
+	
+	@Test
+	public void testGetMillisecondsBad() {
+		assertThrows(StringIndexOutOfBoundsException.class, ()-> {Time.getMilliseconds("10:00:00");});
+	}
+	
+	// ==========================================================================
+	
+	@ParameterizedTest
+	@ValueSource(strings = { "05:05:05", "23:59:59", "10:15:05" })
+	public void testGetSecondsGoodAndBoundary(String candidate) {
+		int seconds = Time.getSeconds(candidate);
+		if (candidate.equals("23:59:59"))
+			assertTrue("The seconds were not calculated properly", (seconds==59));
+		else
+			assertTrue("The seconds were not calculated properly", (seconds==5));
 		
 	}
 	
-	/**
-	 * A method to check that the method will still work properly on boundary input
-	 */
 	@Test
-	void testGetTotalSecondsBoundary() 
-	{
-		int seconds = Time.getTotalSeconds("00:00:00");
-		assertTrue("The seconds were not calculated properly", seconds==0);
+	public void testGetSecondsBad() {
+		assertThrows(StringIndexOutOfBoundsException.class, ()-> {Time.getSeconds("10:00");});
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = { "05:00:00", "05:15:15", "05:59:59" })
-	void testGetTotalHours(String candidate) 
-	{
-		int hours = Time.getTotalHours(candidate);
-		assertTrue("The hours were not calculated properly", hours ==5);
+	@ValueSource(strings = { "05:05:05", "23:59:59", "10:05:15" })
+	public void testGetTotalMinutesGoodAndBoundary(String candidate) {
+		int minutes = Time.getTotalMinutes(candidate);
+		if (candidate.equals("23:59:59"))
+			assertTrue("The minutes were not calculated properly", (minutes==59));
+		else
+			assertTrue("The minutes were not calculated properly", (minutes==5));
 	}
+	
+	@Test
+	public void testGetTotalMinutesBad() {
+		assertThrows(StringIndexOutOfBoundsException.class, ()-> {Time.getTotalMinutes("10");});
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "05:05:05", "23:59:59", "05:10:15" })
+	public void testGetTotalHoursGoodAndBoundary(String candidate) {
+		int hours = Time.getTotalHours(candidate);
+		if (candidate.equals("23:59:59"))
+			assertTrue("The hours were not calculated properly", (hours==23));
+		else
+			assertTrue("The hours were not calculated properly", (hours==5));
+	}
+	
+	@Test
+	public void testGetTotalHoursBad() {
+		assertThrows(StringIndexOutOfBoundsException.class, ()-> {Time.getTotalHours("");});
+	}
+	
 }
